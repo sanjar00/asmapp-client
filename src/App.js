8b442ecebd72e524fb6fd56640ab54ede57253fe
sanjar00 <klinap@gmail.com>
2025-05-02 16:12:08 +0500

    Fix Netlify build issues with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { CssBaseline, Container } from '@mui/material';
 import Login from './components/Login';
 import AdminDashboard from './components/Admin/AdminDashboard';
